70cbcc2b5f7daabbf074dcc4ef86ffe17c692272
vikijai <vigneshjai98@gmail.com>
2025-03-31 23:16:09 +0530

    add docker file

--- server/index.js
+++ server/index.js
@@ -112,5 +112,5 @@ app.post("/compile", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
