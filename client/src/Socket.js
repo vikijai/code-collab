6a4abffe2e996894eb57fd870a0d4d88a0d0d062
vikijai <vigneshjai98@gmail.com>
2025-03-31 23:55:13 +0530

    socket change

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -7,6 +7,6 @@ export const initSocket = async () =>{
         timeout: 10000,
         transports: ['websocket'],
     };
-    const socket = io("http://localhost:5000", options);
+    const socket = io(process.env.REACT_APP_BACKEND_URL, options);
     return socket;
 }
